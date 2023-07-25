cb5abc4e02281f072527751ced4a2bebc4905274
Rishabh <rishabhlakhaiyar27@gmail.com>
2023-07-25 12:47:38 +0530

    navbar competed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import "./Navbar.css";
 import {RiMenu3Line,RiCloseLin} from "react-icons";
+import logo from "../../assets/logo.svg";
+
+
 const Navbar = () => {
   return (
     <div className="gpt3__navbar">
@@ -8,9 +11,22 @@ const Navbar = () => {
         <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo"/>
         </div>
+        <div className="gpt3__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">library</a></p>
+        </div>
+      </div>
+      <div className='gpt3__navbar-sign'>
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
       </div>
     </div>
   )
 }
+
+
 
 export default Navbar
